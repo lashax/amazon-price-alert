b4f3b66f829954ea0e4617646485f667c290a60e
lashax <repeop0@gmail.com>
2021-05-21 00:53:34 +0400

    Update user_input.py

--- amazon/user_input.py
+++ amazon/user_input.py
@@ -1,7 +1,9 @@
+# These urls are just examples
 AMAZON_URLS = [
-    'https://www.amazon.com/GF65-Thin-10UE-091-i7-10750H-RTX3060/dp/B08WFXCKDK/ref=sr_1_5?dchild=1&keywords=laptop&qid=1621540588&sr=8-5']
+    'https://www.amazon.com/GF65-Thin-10UE-091-i7-10750H-RTX3060/dp/B08WFXCKDK/ref=sr_1_5?dchild=1&keywords=laptop&qid=1621540588&sr=8-5',
+    'https://www.amazon.com/Premonition-Pandemic-Story-Michael-Lewis-ebook/dp/B08V91YY8R/ref=tmm_kin_swatch_0?_encoding=UTF8&qid=&sr=']
 
 # MongoDB Information
-MONGO_URI = 'mongodb://localhost:27017'
-MONGO_DATABASE = 'Amazon'
-COLLECTION = 'Price Tracker'
+MONGO_URI = 'YOUR_MONGO_URI'
+MONGO_DATABASE = 'MONGO_DATABASE_NAME'
+COLLECTION = 'COLLECTION_NAME'
